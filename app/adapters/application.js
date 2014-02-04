4ebf045a1615f6762fcafb28d3d433a91c083c0c
erkarl <karl@karlranna.com>
2014-02-04 19:14:08 +0200

    Swap to production API endpoint

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,11 @@
 // export default DS.FixtureAdapter.extend();
+/*
 export default DS.RESTAdapter.extend({
   host: 'http://127.0.0.1:5000/api'
+});
+*/
+
+// Production
+export default DS.RESTAdapter.extend({
+  host: 'http://flask-restful-todo.herokuapp.com/api'
 });
