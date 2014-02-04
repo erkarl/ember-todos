765a6d731c9ef804ef40cc88573e22c5c8b64a0f
erkarl <karl@karlranna.com>
2014-02-04 22:52:23 +0200

    Add Travis

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,11 +1,11 @@
 // export default DS.FixtureAdapter.extend();
-/*
 export default DS.RESTAdapter.extend({
   host: 'http://127.0.0.1:5000/api'
 });
-*/
 
 // Production
+/*
 export default DS.RESTAdapter.extend({
   host: 'http://todo-api.karlranna.com/api'
 });
+*/
